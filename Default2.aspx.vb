d71839a05e5a1af635b3bf97f2d27e3b483728b1
kvrdotnet <33499539+kvrdotnet@users.noreply.github.com>
2019-07-07 23:26:19 +0530

    Add files via upload

---
+++ Default2.aspx.vb
@@ -0,0 +1,59 @@
+﻿
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.IO
+Imports System.Web.UI.WebControls
+Partial Class Default2
+    Inherits System.Web.UI.Page
+
+    Private con As New SqlConnection("Data Source=xxxx;Integrated Security=true;Initial Catalog=xxxx")
+    Protected Sub ImgBtn_Click(sender As Object, e As ImageClickEventArgs)
+        Dim ImgBtn As ImageButton = TryCast(sender, ImageButton)
+        Dim gvrow As GridViewRow = TryCast(ImgBtn.NamingContainer, GridViewRow)
+        Dim filePath As String = gvDetails.DataKeys(gvrow.RowIndex).Value.ToString()
+        Response.ContentType = "image/jpg"
+        Response.AddHeader("Content-Disposition", "attachment;filename=""" & filePath & """")
+        Response.TransmitFile(Server.MapPath(filePath))
+        Response.[End]()
+    End Sub
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If Not Page.IsPostBack Then
+            BindGridviewData()
+        End If
+    End Sub
+    Private Sub BindGridviewData()
+        con.Open()
+        Dim cmd As New SqlCommand("select * from FilesTable", con)
+        Dim da As New SqlDataAdapter(cmd)
+        Dim ds As New DataSet()
+        da.Fill(ds)
+        con.Close()
+        gvDetails.DataSource = ds
+        gvDetails.DataBind()
+    End Sub
+
+    Protected Sub btnUpload_Click(sender As Object, e As EventArgs) Handles btnUpload.Click
+       
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim filename As String = Path.GetFileName(fileUpload1.PostedFile.FileName)
+        Dim ext As String = Path.GetExtension(fileUpload1.FileName)
+        'Response.Write(ext)
+        If ext.ToLower() = ".xlsx" Or ext.ToLower() = ".xls" Then
+            fileUpload1.SaveAs(Server.MapPath("Files/" & filename))
+            con.Open()
+            Dim cmd As New SqlCommand("insert into FilesTable(FileName,FilePath) values(@Name,@Path)", con)
+            cmd.Parameters.AddWithValue("@Name", filename)
+            cmd.Parameters.AddWithValue("@Path", "Files/" & filename)
+            cmd.ExecuteNonQuery()
+            con.Close()
+            BindGridviewData()
+
+        Else
+            Page.RegisterStartupScript("kk", "<script>alert('Invalid file selection')</script>")
+        End If
+        
+    End Sub
+End Class
---
+++ Default3.aspx.vb
@@ -0,0 +1,34 @@
+﻿
+Partial Class Default3
+    Inherits System.Web.UI.Page
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim date1Day As String = Me.t1.Text.Remove(2)
+        Dim date2Day As String = Me.t2.Text.Remove(2)
+        Dim date1Month As String = Me.t1.Text.Substring(3, 2)
+        Dim date2Month As String = Me.t2.Text.Substring(3, 2)
+        Dim date1Year As String = Me.t1.Text.Substring(6)
+        Dim date2Year As String = Me.t2.Text.Substring(6)
+        Dim d1 As DateTime = Convert.ToDateTime(date1Month + "/" + date1Day + "/" + date1Year)
+        Dim d2 As DateTime = Convert.ToDateTime(date2Month + "/" + date2Day + "/" + date2Year)
+        Response.Write(date1Day)
+        Response.Write(date1Month)
+        Response.Write(date1Year)
+        Response.Write(date2Day)
+        Response.Write(date2Month)
+        Response.Write(date2Year)
+        'If d1 > d2 Then
+
+        '    ClientScript.RegisterStartupScript(Me.GetType(), "Alert", "alert('Date1isgreater')", True)
+
+        'ElseIf d1 = d2 Then
+
+        '    ClientScript.RegisterStartupScript(Me.GetType(), "Alert", "alert('Botharesame')", True)
+
+        'Else
+
+        '    ClientScript.RegisterStartupScript(Me.GetType(), "Alert", "alert('Date2isgreater')", True)
+        'End If
+
+    End Sub
+End Class
